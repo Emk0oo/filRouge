abae7a20f8aad735e3235f3d73cd596e203c55e9
Javor Emir <emir.javor@my-digital-school.org>
2023-10-23 11:09:53 +0200

    [ADD] définition class Univers

---
+++ src/back/class/univers.js
@@ -0,0 +1,9 @@
+class Univers{
+    constructor(idUnivers, nom, description, nbPersonnage, idPersonnage){
+        this.idUnivers = idUnivers;
+        this.nom = nom;
+        this.description = description;
+        this.nbPersonnage = nbPersonnage;
+        this.idPersonnage = idPersonnage;
+    }
+}
