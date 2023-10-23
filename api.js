93022a17ebf206e9ebf3c9490e2a31077476c767
Javor Emir <emir.javor@my-digital-school.org>
2023-10-23 16:45:04 +0200

    [ADD] utilisateur/add

--- api.js
+++ api.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const app = express();
 const mysql = require("mysql2");
+const bodyParser = require("body-parser");
 const port = 3000;
+
+app.use(express.json());
 
 const connection = mysql.createConnection({
   host: "localhost",
@@ -14,12 +17,125 @@ app.get("/hello", (req, res) => {
   res.send("Hello World!");
 });
 
+app.post("/utilisateurs/add", (req, res) => {
+  // var { id, nom, prenom, pseudo, mdp, id_message } = req.body;
+  // var sql= `INSERT INTO utilisateurs (id, nom, prenom, pseudo, mdp, id_message) VALUES (?, ?, ?, ?, ?, ?)`;
+  // var tab=[id, nom, prenom, pseudo, mdp, id_message];
+  // connection.query(sql, tab, (err, rows, fields) => {
+  //   if (err) throw err;
+  //   res.status(200).json(rows);
+  // }); 
+  let body = req.body;
+  const data = req.body;
+  const requete = `INSERT INTO utilisateurs (id, nom, prenom, pseudo, mdp, id_message) VALUES ('${body.id}', '${body.nom}', '${body.prenom}', '${body.pseudo}', '${body.mdp}', '${body.id_message}')`;
+  connection.query(requete, function (err, result) {
+    if (err) throw err;
+    res.send("1 record inserted");
+  });
+});
+
+app.put("/utilisateurs/update/:id", (req, res) => {
+  let body = req.body;
+  const data = req.body;
+  const requete = `UPDATE utilisateurs SET nom = '${body.nom}', prenom = '${body.prenom}', pseudo = '${body.pseudo}', mdp = '${body.mdp}', id_message = '${body.id_message}' WHERE id = '${req.params.id}'`;
+  connection.query(requete, function (err, result) {
+    if (err) throw err;
+    res.send("1 record updated");
+  });
+});
+
+app.get("utilisateurs", (req, res) => {
+  connection.query("SELECT * FROM users", (err, rows, fields) => {
+    if (err) throw err;
+    res.send(rows);
+  });
+});
+
+app.get("/utilisateurs/:id", (req, res) => {
+  connection.query(
+    "SELECT * FROM utilisateurs WHERE id= ?",
+    [req.params.id],
+    (err, rows, fields) => {
+      if (err) throw err;
+      res.send(rows);
+    }
+  );
+});
+
+app.post("/univers/add", (req, res) => {
+  let body = req.body;
+  const data = req.body;
+  const requete = `INSERT INTO univers (id, description, id_utilisateurs, nom, id_images, nb_perso) VALUES ('${body.id}', '${body.description}', '${body.id_utilisateurs}', '${body.nom}', '${body.id_images}', '${body.nb_perso}')`;
+  connection.query(requete, function (err, result) {
+    if (err) throw err;
+    res.send("1 record inserted");
+  });
+});
+
 app.get("/univers", (req, res) => {
   connection.query("SELECT * FROM univers", (err, rows, fields) => {
     if (err) throw err;
     res.send(rows);
   });
 });
+
+app.get("/univers/:id", (req, res) => {
+  connection.query(
+    "SELECT * FROM univers WHERE id= ?",
+    [req.params.id],
+    (err, rows, fields) => {
+      if (err) throw err;
+      res.send(rows);
+    }
+  );
+});
+
+app.get("/personnages", (req, res) => {
+  connection.query("SELECT * FROM personnages", (err, rows, fields) => {
+    if (err) throw err;
+    res.send(rows);
+  });
+});
+
+app.post("/personnages/add", (req, res) => {
+  let body = req.body;
+  const data = req.body;
+  const requete = `INSERT INTO personnages (id, nom, id_images, id_messages) VALUES ('${body.id}', '${body.nom}', '${body.id_images}', '${body.id_messages}')`;
+  connection.query(requete, function (err, result) {
+    if (err) throw err;
+    res.send("1 record inserted");
+  });
+});
+
+app.get("/personnages/:id", (req, res) => {
+  connection.query(
+    "SELECT * FROM personnages WHERE id= ?",
+    [req.params.id],
+    (err, rows, fields) => {
+      if (err) throw err;
+      res.send(rows);
+    }
+  );
+});
+
+app.get("/images", (req, res) => {
+  connection.query("SELECT * FROM images", (err, rows, fields) => {
+    if (err) throw err;
+    res.send(rows);
+  });
+});
+
+app.post("/images/add", (req, res) => {
+  let body = req.body;
+  const data = req.body;
+  const requete = `INSERT INTO images (id, url) VALUES ('${body.id}', '${body.url}')`;
+  connection.query(requete, function (err, result) {
+    if (err) throw err;
+    res.send("1 record inserted");
+  });
+});
+
+app.get("/images/:id", (req, res) => {});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
@@ -33,10 +149,3 @@ connection.connect(function (error) {
   }
 });
 
-/*connection.query('SELECT 1 + 1 AS solution', (err, rows, fields) => {
-    if (err) throw err
-  
-    console.log('The solution is: ', rows[0].solution)
-  })
-  
-  connection.end()*/
