67beadd98e4b58af7ba9003fe2d3195f131d989c
Javor Emir <emir.javor@my-digital-school.org>
2023-10-24 09:50:49 +0200

    [UPD] route univers etc

--- api.js
+++ api.js
@@ -17,20 +17,23 @@ app.get("/hello", (req, res) => {
   res.send("Hello World!");
 });
 
+//////////////////////////////////////////////
+//////////////UTILISATEURS////////////////////
+//////////////////////////////////////////////
+
 app.post("/utilisateurs/add", (req, res) => {
-  // var { id, nom, prenom, pseudo, mdp, id_message } = req.body;
-  // var sql= `INSERT INTO utilisateurs (id, nom, prenom, pseudo, mdp, id_message) VALUES (?, ?, ?, ?, ?, ?)`;
-  // var tab=[id, nom, prenom, pseudo, mdp, id_message];
-  // connection.query(sql, tab, (err, rows, fields) => {
-  //   if (err) throw err;
-  //   res.status(200).json(rows);
-  // });
-  let body = req.body;
-  const data = req.body;
-  const requete = `INSERT INTO utilisateurs (id, nom, prenom, pseudo, mdp, id_message) VALUES ('${body.id}', '${body.nom}', '${body.prenom}', '${body.pseudo}', '${body.mdp}', '${body.id_message}')`;
-  connection.query(requete, function (err, result) {
-    if (err) throw err;
-    res.send("1 record inserted");
+  const userData = req.body;
+  const sql = "INSERT INTO utilisateurs (id, nom, prenom, pseudo, mdp, id_message) VALUES (?, ?, ?, ?, ?, ?)";
+  const values = [userData.id, userData.nom, userData.prenom, userData.pseudo, userData.mdp, userData.id_message];
+
+  connection.query(sql, values, (err, result) => {
+    if (err) {
+      console.error("Erreur lors de l'insertion :", err);
+      res.status(500).json({ error: "Erreur lors de l'insertion" });
+    } else {
+      console.log("Enregistrement inséré avec succès !");
+      res.status(200).json({ message: "Enregistrement inséré avec succès" });
+    }
   });
 });
 
@@ -43,13 +46,13 @@ app.put("/utilisateurs/update/:id", (req, res) => {
     res.send("1 record updated");
   });
 });
-//////////////////////////////////////////////
-//////////////UTILISATEURS////////////////////
-//////////////////////////////////////////////
-app.get("utilisateurs", (req, res) => {
-  connection.query("SELECT * FROM users", (err, rows, fields) => {
+
+app.get("/utilisateurs", (req, res) => {
+
+  let requete= "SELECT * FROM utilisateurs";
+  connection.query(requete, (err, rows, fields) => {
     if (err) throw err;
-    res.send(rows);
+    res.status(200).json(rows);
   });
 });
 
@@ -76,44 +79,62 @@ app.get("/univers", (req, res) => {
 });
 
 app.post("/univers/add", (req, res) => {
-  let body = req.body;
-  const data = req.body;
-  const requete = `INSERT INTO univers (id, description, id_utilisateurs, nom, id_images, nb_perso) VALUES ('${body.id}', '${body.description}', '${body.id_utilisateurs}', '${body.nom}', '${body.id_images}', '${body.nb_perso}')`;
-  connection.query(requete, function (err, result) {
-    if (err) throw err;
-    res.send("1 record inserted");
+  const universData = req.body;
+  const sql = "INSERT INTO univers (description, id_utilisateurs, nom, id_images, nb_perso) VALUES (?, ?, ?, ?, ?)";
+  const values = [universData.description, universData.id_utilisateurs, universData.nom, universData.id_images, universData.nb_perso];
+  console.log(values);
+  connection.query(sql, values, (err, result) => {
+    if (err) {
+      console.error("Erreur lors de l'insertion :", err);
+      res.status(500).json({ error: "Erreur lors de l'insertion" });
+    } else {
+      console.log("Enregistrement inséré avec succès !");
+      res.status(200).json({ message: "Enregistrement inséré avec succès" });
+    }
   });
-});
-
-app.post("/univers/:id/characters", (req, res) => {
-  let body = req.body;
-  res.send(req.body);
-  let sql = `INSERT INTO personnages (nom, id_images, id_messages, id_univers) VALUES ('${body.nom}', '${body.id_images}', '${body.id_messages}' , ${req.params.id})`;
-  connection.query(sql, function (err, result) {
-    if (err) throw err;
-    console.log("1 record inserted");
-  });
-  res.send("Personnage ajouté avec succès.");
 });
 
 app.get("/univers/:id", (req, res) => {
   connection.query(
-    "SELECT * FROM univers WHERE id= ?",
-    [req.params.id],
-    (err, rows, fields) => {
+    "SELECT * FROM univers WHERE id= ?",[req.params.id],(err, rows, fields) => {
       if (err) throw err;
       res.send(rows);
     }
   );
 });
 
+app.post("/univers/:id/characters", (req, res) => {
+  const personnagesData = req.body;
+  let sql="INSERT INTO personnages (nom, id_images, id_messages, id_univers) VALUES (?, ?, ?, ?)";
+  const values = [personnagesData.nom, personnagesData.id_images, personnagesData.id_messages, personnagesData.id_univers];
+  console.log(values);
+  connection.query(sql, values, (err, result) => {
+    if (err) {
+      console.error("Erreur lors de l'insertion :", err);
+      res.status(500).json({ error: "Erreur lors de l'insertion" });
+    } else {
+      console.log("Enregistrement inséré avec succès !");
+      res.status(200).json({ message: "Enregistrement inséré avec succès" });
+    }
+  });
+});
+
 app.put("/univers/update/:id", (req, res) => {
-  let body = req.body;
-  const data = req.body;
-  const requete = `UPDATE univers SET description = '${body.description}', id_utilisateurs = '${body.id_utilisateurs}', nom = '${body.nom}', id_images = '${body.id_images}', nb_perso = '${body.nb_perso}' WHERE id = '${req.params.id}'`;
-  connection.query(requete, function (err, result) {
-    if (err) throw err;
-    res.send("1 record updated");
+  const body = req.body;
+  const id = req.params.id;
+
+  // Utilisez une requête préparée pour mettre à jour les données
+  const sql = "UPDATE univers SET description = ?, id_utilisateurs = ?, nom = ?, id_images = ?, nb_perso = ? WHERE id = ?";
+  const values = [body.description, body.id_utilisateurs, body.nom, body.id_images, body.nb_perso, id];
+
+  connection.query(sql, values, (err, result) => {
+    if (err) {
+      console.error("Erreur lors de la mise à jour :", err);
+      res.status(500).json({ error: "Erreur lors de la mise à jour" });
+    } else {
+      console.log("Enregistrement mis à jour avec succès !");
+      res.status(200).json({ message: "Enregistrement mis à jour avec succès" });
+    }
   });
 });
 
