791af67bacbb680ce2b277fd04198a5781a9dd4f
Javor Emir <emir.javor@my-digital-school.org>
2023-11-06 15:21:59 +0100

    [ADD] singleton

--- src/back/mysql.js
+++ src/back/mysql.js
@@ -1,5 +1,21 @@
 const mysql = require("mysql2");
 
+class MysqlSingleton{
+  static instance;
+
+  static getInstance(){
+    if(!MysqlSingleton.instance){
+      MysqlSingleton.instance= mysql.createConnection({
+        host: "localhost",
+        user: "admin",
+        password: "abcde",
+        database: "fil_rouge",
+      });
+    }
+    return MysqlSingleton.instance;
+  }
+}
+
 const connection = mysql.createConnection({
   host: "localhost",
   user: "admin",
